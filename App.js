92579f5ee7a0a1dcf1ec3c13c1ed335a8d019ea7
Nilesh <nilesh.tudu@nirmitee.io>
2023-09-15 15:38:27 +0530

    Home screen added

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { store } from './src/store/configureStore';
+import { StatusBar, Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { Provider } from 'react-redux';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app test!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar translucent={true} />
+      <Provider store={store}>
+        <PaperProvider>
+          <AppNavigator />
+        </PaperProvider>
+      </Provider>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
